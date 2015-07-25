7da1bd2c19f396630bf30fcb1847bab107dbe3a6
Gelso Rocha <gelsorocha@hotmail.com>
2015-07-25 01:52:44 -0300

    add udp and tcp example

--- ex1.py
+++ ex1.py
@@ -1,7 +1,7 @@
 import socket
 
-target_host = 'www.google.com.br'
-target_port = 80
+target_host = '0.0.0.0'
+target_port = 9999
 
 #create a socket object
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,7 +10,7 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((target_host, target_port))
 
 #send some data
-client.send("GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+client.send("ABCDE")
 
 # receive some data
 response = client.recv(4096)
